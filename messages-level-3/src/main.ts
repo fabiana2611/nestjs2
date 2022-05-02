1a3e25e9f543e2ad46116c7f0324e281b9d5279b
Bia Freire <fabiana2611@hotmail.com>
2022-05-02 14:52:41 +0200

    adding security library

--- messages-level-3/src/main.ts
+++ messages-level-3/src/main.ts
@@ -3,6 +3,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
 import * as session from 'express-session';
 import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
+import * as helmet from 'helmet';
 
 //#1
 // import cookieSession from 'cookie-session';
@@ -11,6 +13,9 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+
+  app.use(helmet);
+
   app.useGlobalPipes(new ValidationPipe());
 
   //#1
@@ -32,6 +37,12 @@ async function bootstrap() {
   );
 
   app.use(cookieParser());
+
+  //this middleware requires either session middleware or cookie-parser to be initialized first
+  app.use(csurf);
+
+  // CORS preventions to REST endpoints
+  app.enableCors();
 
   await app.listen(3001);
 }
--- messages-level-3/src/messages/messages.module.ts
+++ messages-level-3/src/messages/messages.module.ts
@@ -5,10 +5,26 @@ import { MessagesRepository } from './messages.repository';
 import { MessagesMiddleware } from './middleware/messages.middleware';
 import { AuthModule } from '../auth/auth.module';
 import { ConfigModule } from '../config/config.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+
 
 @Module({
-  imports: [AuthModule, ConfigModule.register({ attribute: 'staging' })],
-  providers: [MessagesService, MessagesRepository],
+  imports: [
+    AuthModule,
+    ConfigModule.register({ attribute: 'staging' }),
+    ThrottlerModule.forRoot({
+      //time to live
+      ttl: 60,
+      // the maximum number of requests within the ttl, for the routes of your application that are guarded
+      limit: 10,
+    }),
+  ],
+  providers: [
+    MessagesService,
+    MessagesRepository,
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+  ],
   /**
    *   Study Notes:
    *   using middleware insted of interceptor because of the order of execution
